7b3b526fa3323c13368cfe948fa421f3bb35ba00
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2020-10-10 01:01:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app=express();
+let items=["Buy Food","Cook Food","Eat Food"];
+let workItems=[];
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.get("/", function(req,res){
+
+  let today=new Date();
+
+  let options = {
+    weekday: "long",
+    day:"numeric",
+    month: "long"
+  };
+
+  let day= today.toLocaleDateString("en-US",options);
+
+  // if(currentDay===6 || currentDay===0){
+  //   day = "Weekend";
+  //   //res.sendFile(__dirname+"/weekend.html");
+  // }else{
+  //       day="Weekday";
+  //       //res.sendFile(__dirname + "/weekend.html");
+  // }
+
+
+  res.render("list",{
+    listTitle: day, newListItems: items
+  });
+});
+
+// when submit button is clicked it gets linked to where the item is
+// input is stored in the item and its redirected to home route
+app.post("/",function(req,res){
+
+
+  let item=req.body.newItem;
+
+  if(req.body.list=="Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  }
+  else{
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req,res){
+  res.render("list",{listTitle: "Work List",newListItems:workItems});
+});
+
+app.get("/about", function(req,res){
+  res.render("about");
+});
+
+// app.post("/work", function(req,res){
+//   let item=req.body.newItem;
+//   workItems.push(item);
+//   res.redirect("/work");
+// })
+
+app.listen(3000,function(){
+  console.log("Server started on port 3000");
+});
